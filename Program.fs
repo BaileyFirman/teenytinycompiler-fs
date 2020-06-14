aac44fd43603c7dfb4f1c489af26a489c1eeb16d
BaileyFirman <bailey.firman@nintex.com>
2020-06-14 22:36:43 +1200

    Added new input to main along with printing of the current token

--- Program.fs
+++ Program.fs
@@ -5,16 +5,26 @@ let main argv =
     let printc char =
         printf "%c\n" char
 
+    let prints string =
+        printf "%s\n" string
+
     let rec parseLoop (inputArray: char[]) =
-        let currChar = Lexer.nextChar(inputArray)
-        printc currChar
+        let currToken =
+            inputArray
+            |> Lexer.nextChar
+            |> Lexer.getToken
+        
+        let currTokenType = currToken.Type.ToString()
+
+        prints currTokenType
 
         let nextChar = Lexer.peek(inputArray)
         match nextChar with 
         | '0' -> printf ">>> %s" "DONE"
         | _ -> parseLoop (inputArray.[1..]) 
 
-    let input = "LET foobar = 123".ToCharArray()
+    // let input = "LET foobar = 123".ToCharArray()
+    let input = "+- */".ToCharArray()
 
     parseLoop input
     0
