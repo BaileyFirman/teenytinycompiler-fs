62225a0b05dd4ae87361649a8a73980719429fa5
BaileyFirman <bailey.firman@nintex.com>
2020-06-14 21:27:10 +1200

    Added parseLoop to main

--- Program.fs
+++ Program.fs
@@ -1,5 +1,20 @@
-open System
+open Lexer
 
 [<EntryPoint>]
 let main argv =
+    let printc char =
+        printf "%c\n" char
+
+    let rec parseLoop (inputArray: char[]) =
+        let currChar = Lexer.nextChar(inputArray)
+        printc currChar
+
+        let nextChar = Lexer.peek(inputArray)
+        match nextChar with 
+        | '0' -> printf ">>> %s" "DONE"
+        | _ -> parseLoop (inputArray.[1..]) 
+
+    let input = "LET foobar = 123".ToCharArray()
+
+    parseLoop input
     0
