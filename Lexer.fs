a1fd87d8e14fb1c19873c77abade2130c71f4755
BaileyFirman <bailey.firman@nintex.com>
2020-06-14 22:36:01 +1200

    Added TokenType and Token to Lexer along with getToken

--- Lexer.fs
+++ Lexer.fs
@@ -1,13 +1,66 @@
 namespace Lexer
 
-module Lexer =
-    let nextChar (tokenArray: char[]): char =
-        tokenArray.[0]
+type TokenType =
+    | EOF = -1
+    | NEWLINE = 0
+    | NUMBER = 1
+    | IDENT = 2
+    | STRING = 3
+    | LABEL = 101
+    | GOTO = 102
+    | PRINT = 103
+    | INPUT = 104
+    | LET = 105
+    | IF = 106
+    | THEN = 107
+    | ENDIF = 108
+    | WHILE = 109
+    | REPEAT = 110
+    | ENDWHILE = 111
+    | EQ = 201
+    | PLUS = 202
+    | MINUS = 203
+    | ASTERISK = 204
+    | SLASH = 205
+    | EQEQ = 206
+    | NOTEQ = 207
+    | LT = 208
+    | LTEQ = 209
+    | GT = 210
+    | GTEQ = 211
 
-    let peek (tokenArray: char[]): char =
+type Token = { Text: char; Type: TokenType }
+
+module Lexer =
+    let abort message =
+        let failMessage = sprintf "%s%s" "Lexing error. " message
+        failwith failMessage
+
+    let nextChar (tokenArray: char []): char = tokenArray.[0]
+
+    let peek (tokenArray: char []): char =
         match tokenArray.Length = 1 with
         | true -> '0'
         | false -> tokenArray.[1]
 
-    let abort message =
-        printf "%s" message
+    let getToken (currChar: char) =
+        match currChar with
+        | '+' ->
+            { Text = currChar
+              Type = TokenType.PLUS }
+        | '-' ->
+            { Text = currChar
+              Type = TokenType.MINUS }
+        | '*' ->
+            { Text = currChar
+              Type = TokenType.ASTERISK }
+        | '/' ->
+            { Text = currChar
+              Type = TokenType.SLASH }
+        | '\n' ->
+            { Text = currChar
+              Type = TokenType.NEWLINE }
+        | '0' ->
+            { Text = currChar
+              Type = TokenType.EOF }
+        | _ -> sprintf "%s%c" "Unknown Token: " currChar |> abort
