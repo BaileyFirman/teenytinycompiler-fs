bfd4416a377ede93794f664dd8f4e6bd057d1f53
BaileyFirman <bailey.firman@nintex.com>
2020-06-15 16:22:49 +1200

    Updated main moving print helper functions outside and clarifying parsing in main loop

--- Program.fs
+++ Program.fs
@@ -1,44 +1,41 @@
 open Lexer
 
+let printc char =
+    printf "%c\n" char
+
+let prints string =
+    printf "%s\n" string
+
+let printt (token: Token) =
+    let tokenTypeString = token.Type.ToString()
+    printf "%s\n" tokenTypeString
+
+let printArray (array: char[]) =
+    let arrayAsString = System.String (array)
+    printf "\t\t\t\t DEBUG >>> %s\n" arrayAsString
+
 [<EntryPoint>]
 let main argv =
-    let printc char =
-        printf "%c\n" char
-
-    let prints string =
-        printf "%s\n" string
-
-    let printt (token: Token) =
-        let tokenTypeString = token.Type.ToString()
-        printf "%s\n" tokenTypeString
 
     let rec parseLoop (inputArray: char[]) =
-        let currToken =
-            inputArray
-            |> Lexer.nextChar
+        // Skip until the next character isn't whitespace
+        let characterArray = Lexer.skipWhiteSpace inputArray
+        let currentChar = Lexer.nextChar characterArray
+        let nextChar = Lexer.peek characterArray
 
-        let y = Lexer.getToken(currToken, inputArray)
+        let skipOffset = Lexer.skipOffset (currentChar, nextChar)
 
-        printt y
-
-        let nextChar = Lexer.peek(inputArray)
-        let offset = Lexer.skipOffset(currToken, nextChar)
-        
-        let newInputArray =
-            match inputArray.Length < offset with
-            | true -> [||]
-            | false -> inputArray.[offset..]
+        Lexer.getToken (currentChar, characterArray)
+        |> printt
+        |> ignore
 
         match nextChar with 
-        | '0' -> printf ">>> %s" "DONE"
-        | _ ->
-            match newInputArray.Length = 0 with
-            | true -> printf ">>> %s" "DONE"
-            | false -> parseLoop (newInputArray)
+        | '0' -> printf "\t\t\t\t DEBUG >>> %s" "DONE"
+        | _ -> parseLoop (characterArray.[skipOffset..])
 
     // let input = "LET foobar = 123".ToCharArray()
     // let input = "+- */\n".ToCharArray()
-    let input = "+- */ >>= = !=".ToCharArray()
+    let input = "+- */ >>= = !=\n0".ToCharArray()
 
     parseLoop input
     0
