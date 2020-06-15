5afb151e55d5e9a54dbbfa83efddfc623dd0781e
BaileyFirman <bailey.firman@nintex.com>
2020-06-15 17:40:25 +1200

    Updated parseLoop to correctly pre-process at each parsing recursive step

--- Program.fs
+++ Program.fs
@@ -19,15 +19,27 @@ let main argv =
 
     let rec parseLoop (inputArray: char[]) =
         // Skip until the next character isn't whitespace
-        let characterArray = Lexer.skipWhiteSpace inputArray
+        let removedWhitespace = Lexer.skipWhiteSpace inputArray
+        // Remove comments
+        let removedComments =
+            match removedWhitespace.[0] with
+            | '#' -> Lexer.skipComment removedWhitespace
+            | _ -> removedWhitespace
+        
+        let characterArray = Lexer.skipWhiteSpace removedComments 
+
+        // Get our current and next chars
         let currentChar = Lexer.nextChar characterArray
         let nextChar = Lexer.peek characterArray
 
-        let skipOffset = Lexer.skipOffset (currentChar, nextChar)
-
-        Lexer.getToken (currentChar, characterArray)
+        let currentToken = Lexer.getToken (currentChar, characterArray)
+        
+        currentToken
         |> printt
         |> ignore
+
+        // How far should we skip?
+        let skipOffset = Lexer.skipOffset (currentToken)
 
         match nextChar with 
         | '0' -> printf "\t\t\t\t DEBUG >>> %s" "DONE"
@@ -35,7 +47,9 @@ let main argv =
 
     // let input = "LET foobar = 123".ToCharArray()
     // let input = "+- */\n".ToCharArray()
-    let input = "+- */ >>= = !=\n0".ToCharArray()
+    // let input = "+- */ >>= #a comment\n= !=\n0".ToCharArray()
+    // let input = "+- # This is a comment!\n */".ToCharArray()
+    let input = "+- \"This is a string\" # This is a comment!\n */".ToCharArray()
 
     parseLoop input
     0
