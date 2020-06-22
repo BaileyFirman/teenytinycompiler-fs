3ba151623a800ef8fe4b71fe7b5fbe91f87cec31
BaileyFirman <bailey.firman@nintex.com>
2020-06-22 23:16:08 +1200

    Cleaned up parser and added relevant debug statements

--- Parser.fs
+++ Parser.fs
@@ -10,32 +10,36 @@ module Parser =
     let matchToken current kind =
         match checkToken current kind with
         | true -> true
-        | false -> "Expected" + kind.ToString() + ", got " + current.Type.ToString() |> failwith
-
+        | false ->
+            "Expected"
+            + kind.ToString()
+            + ", got "
+            + current.Type.ToString()
+            |> failwith
 
     let abort message = sprintf "Error. %s" message |> failwith
 
     let rec skipNewLines (tokens: Token []) count =
         printfn "NEWLINE"
         match checkToken tokens.[0] TokenType.NEWLINE with
-        | true -> skipNewLines tokens.[1..] count + 1
-        | false -> count
+        | true -> count
+        | false -> skipNewLines tokens.[1..] count + 1
 
     let private printString (tokens: Token []) =
-        // We need to get a newline offset
-        let z = string tokens.[1].Text
-        printf "STATEMENT-PRINT: %s\n" z
-        // let trailingNewlinesCount = skipNewLines tokens.[2..] 0
-        // trailingNewlinesCount + 2
-        3
+        let tokenText = tokens.[1].Text
+        printf "STATEMENT-PRINT: %s\n" tokenText
+
+        match tokens.[2..].Length = 0 with
+        | true -> 2
+        | false -> skipNewLines tokens.[2..] 3
 
     let private printExpression tokens = 0
 
-    let statement (tokens: Token []) = 
-        let first = tokens.[0]
-        let second = tokens.[1]
+    let statement (tokens: Token []) =
+        let firstToken = tokens.[0]
+        let secondToken = tokens.[1]
 
-        match (first.Type, second.Type) with
+        match (firstToken.Type, secondToken.Type) with
         | (TokenType.PRINT, TokenType.STRING) -> printString tokens
         // | (TokenType.PRINT, _) -> printExpression tokens
         | _ -> "OPPS" |> failwith
