67449d068aee7ea354ef9631a0d017f16fad7059
BaileyFirman <bailey.firman@nintex.com>
2020-08-10 22:11:32 +1200

    Replaced Lexer with BetterLexer

--- Lexer.fs
+++ Lexer.fs
@@ -4,133 +4,151 @@ open Microsoft.FSharp.Core
 open System
 open Types.Tokens
 
-module LexerFuncs =
-    let private abort message =
-        sprintf "Lexing Error. %s" message |> failwith
+module Lexer =
+    let private findChar arr char = arr |> Array.findIndex ((=) char)
 
-    let tokenFromString tokenText tokenType = { Text = tokenText; Type = tokenType }
-    let tokenFromChar (c: char) t = { Text = string c; Type = t }
+    let private blankToken text: TokenType -> Token =
+        (fun tokenType -> { Text = text; Type = tokenType })
 
-    let nextChar (chars: char []) = chars.[0]
+    // Character detection
+    let private isLetter char = Char.IsLetter char
+    let private isDigit char = Char.IsDigit char
+    let private isPoint char = char = '.'
 
-    let peek (chars: char []) =
-        match chars.Length = 1 with
-        | true -> '\u0004'
-        | false -> chars.[1]
+    // Pointer manipulation
+    let private next pointer = pointer + 1
+    let private previous pointer = pointer - 1
 
-    let rec skipWhiteSpace (chars: char []) =
-        match chars.[0] with
-        | ' '
-        | '\t'
-        | '\r' -> skipWhiteSpace chars.[1..]
-        | _ -> chars
+    // Data transformation
+    let private charsToString (chars: char []) = String chars
+    let private combineChars first second = string first + string second
+    let private appendItem array item = Array.append array [| item |]
 
-    let rec skipComment (chars: char []) =
-        match chars.[0] with
-        | '\n' -> chars
-        | _ -> skipComment chars.[1..]
+    // Token creation
+    let private tokenFromString tokenType text = { Text = text; Type = tokenType }
 
-    let skipOffset token =
-        let identifierOrSingle tokenType =
-            match tokenType > 100 || tokenType < 200 with
-            | true -> token.Text.Length
-            | false -> 1
+    let lexCharacterStream (characterStream: char []): Token [] =
 
-        match token.Type with
-        | TokenType.GTEQ
-        | TokenType.LTEQ
-        | TokenType.NOTEQ -> token.Text.Length
-        | TokenType.STRING -> token.Text.Length + 2 // " & "
-        | TokenType.NUMBER -> token.Text.Length + 1
-        | _ -> identifierOrSingle (int token.Type)
+        let rec lexLoop tokens streamPointer: Token [] =
+            let currentCharacter = characterStream.[streamPointer]
 
-    let private buildStringToken chars =
-        let endIndex = chars |> Array.findIndex ((=) '\"')
-        let stringText = String(chars.[..(endIndex - 1)])
-        let invalidStringChars = [| '\r'; '\t'; '\\'; '%' |]
+            let nextCharacter: char =
+                match currentCharacter with
+                | '\u0004' -> '\u0004'
+                | _ -> characterStream.[next streamPointer]
 
-        let stringValid =
-            invalidStringChars
-            |> Array.map stringText.Contains
-            |> Array.sumBy (function
-                | true -> 1
-                | false -> 0) = 0
+            let tokenFromRange startPointer endPointer tokenType: Token =
+                let previousEndPointer = previous endPointer
 
-        match stringValid with
-        | true -> tokenFromString stringText TokenType.STRING
-        | false -> "Illegal character in string." |> abort
+                let streamChars =
+                    characterStream.[startPointer..previousEndPointer]
+                    |> charsToString
 
-    let private handleToken chars =
-        let currChar = nextChar chars
-        let nextChar = peek chars
+                tokenFromString tokenType streamChars
 
-        let abortOnUnknown currChar =
-            sprintf "%s%d" "Unknown Token: " (int currChar)
-            |> abort
+            let streamCharToken tokenType closingCharacter: Token =
+                let nextPointer = next streamPointer
 
-        match (currChar, nextChar) with
-        | ('+', _) -> tokenFromChar currChar TokenType.PLUS
-        | ('-', _) -> tokenFromChar currChar TokenType.MINUS
-        | ('*', _) -> tokenFromChar currChar TokenType.ASTERISK
-        | ('/', _) -> tokenFromChar currChar TokenType.SLASH
-        | ('\n', _) -> tokenFromChar currChar TokenType.NEWLINE
-        | ('\u0004', _) -> tokenFromChar currChar TokenType.EOF
-        | ('=', '=') -> tokenFromString (string currChar + string nextChar) TokenType.EQ
-        | ('=', _) -> tokenFromChar currChar TokenType.EQ
-        | ('>', '=') -> tokenFromString (string currChar + string nextChar) TokenType.GTEQ
-        | ('>', _) -> tokenFromChar currChar TokenType.GT
-        | ('<', '=') -> tokenFromString (string currChar + string nextChar) TokenType.LTEQ
-        | ('<', _) -> tokenFromChar currChar TokenType.LT
-        | ('!', '=') -> tokenFromString (string currChar + string nextChar) TokenType.NOTEQ
-        | ('!', _) -> "Expected !=, got !" |> abort
-        | ('\"', _) -> buildStringToken chars.[1..]
-        | _ -> abortOnUnknown currChar
+                let closingPointer =
+                    let remainingStream = characterStream.[nextPointer..]
 
-    let rec private buildValueFromChars chars newValue matchFunc =
-        let currentChar = nextChar chars
-        let charMatch = matchFunc currentChar
+                    let endPointer =
+                        findChar remainingStream closingCharacter
 
-        match charMatch with
-        | true -> buildValueFromChars chars.[1..] (newValue + (string currentChar)) matchFunc
-        | false -> newValue
+                    endPointer + nextPointer
 
-    let private handleNumberToken chars =
-        let numberString =
-            buildValueFromChars chars "" (fun c -> Char.IsDigit c || c = '.')
+                tokenFromRange nextPointer closingPointer tokenType
 
-        tokenFromString numberString TokenType.NUMBER
+            let singleCharToken tokenType: Token =
+                tokenFromString tokenType (string currentCharacter)
 
-    let private handleAlphaToken chars =
-        let alphaString =
-            buildValueFromChars chars "" Char.IsLetter
+            let doubleCharToken tokenType: Token =
+                let tokenText =
+                    combineChars currentCharacter nextCharacter
 
-        let tokenType =
-            match alphaString with
-            | "LABEL" -> TokenType.LABEL
-            | "GOTO" -> TokenType.GOTO
-            | "PRINT" -> TokenType.PRINT
-            | "INPUT" -> TokenType.INPUT
-            | "LET" -> TokenType.LET
-            | "IF" -> TokenType.IF
-            | "THEN" -> TokenType.THEN
-            | "ENDIF" -> TokenType.ENDIF
-            | "WHILE" -> TokenType.WHILE
-            | "REPEAT" -> TokenType.REPEAT
-            | "ENDWHILE" -> TokenType.ENDWHILE
-            | _ -> TokenType.IDENT
+                tokenFromString tokenType tokenText
 
-        tokenFromString alphaString tokenType
+            let buildSymbolToken (): Token =
+                match currentCharacter, nextCharacter with
+                | ' ', _
+                | '\t', _
+                | '\r', _ -> singleCharToken TokenType.WHITESPACE
+                | '-', _ -> singleCharToken TokenType.MINUS
+                | '!', '=' -> doubleCharToken TokenType.NOTEQ
+                | '!', _ -> "Expected !=" |> failwith
+                | '*', _ -> singleCharToken TokenType.ASTERISK
+                | '/', _ -> singleCharToken TokenType.SLASH
+                | '\"', _ -> streamCharToken TokenType.STRING '\"'
+                | '\n', _ -> singleCharToken TokenType.NEWLINE
+                | '\u0004', _ -> singleCharToken TokenType.EOF
+                | '#', _ -> streamCharToken TokenType.STRING '\n'
+                | '+', _ -> singleCharToken TokenType.PLUS
+                | '<', '=' -> doubleCharToken TokenType.LTEQ
+                | '<', _ -> singleCharToken TokenType.LT
+                | '=', '=' -> doubleCharToken TokenType.EQEQ
+                | '=', _ -> singleCharToken TokenType.EQ
+                | '>', '=' -> doubleCharToken TokenType.GTEQ
+                | '>', _ -> singleCharToken TokenType.GT
+                | _, _ -> "Aborted Lexing" |> failwith
 
-    let getToken chars =
-        let currChar = nextChar chars
+            let buildCharacterToken startPointer: Token =
+                let streamToToken sp ep =
+                    let stringText =
+                        characterStream.[sp..ep] |> charsToString
 
-        let alphaNumTuple =
-            (Char.IsLetter currChar, Char.IsDigit currChar)
+                    let tokenType = matchIdentifier stringText
+                    tokenFromString tokenType stringText
 
-        let handleFunc =
-            match alphaNumTuple with
-            | (false, false) -> handleToken
-            | (true, _) -> handleAlphaToken
-            | (_, true) -> handleNumberToken
+                let rec characterToken startPointer endPointer =
 
-        handleFunc chars
+                    let nextCharacter = characterStream.[endPointer]
+
+                    match isLetter nextCharacter with
+                    | true -> characterToken startPointer <| next endPointer
+                    | false -> streamToToken startPointer <| previous endPointer
+
+                characterToken startPointer <| next startPointer
+
+            let buildNumberToken startPointer: Token =
+                let rec newNumberTokenLoop endPointer: Token =
+                    let nextCharacter = characterStream.[endPointer]
+
+                    match isDigit nextCharacter, isPoint nextCharacter with
+                    | false, false -> tokenFromRange startPointer endPointer TokenType.NUMBER
+                    | _, true ->
+                        let nextCharAfterPointer = characterStream.[next endPointer]
+                        match isDigit nextCharAfterPointer with
+                        | true -> endPointer |> next |> newNumberTokenLoop
+                        | false -> failwith "Illegal Character in number"
+                    | true, _ -> endPointer |> next |> newNumberTokenLoop
+
+                startPointer |> next |> newNumberTokenLoop
+
+            let newToken: Token =
+                match isLetter currentCharacter, isDigit currentCharacter with
+                | false, false -> buildSymbolToken ()
+                | true, _ -> buildCharacterToken streamPointer
+                | _, true -> buildNumberToken streamPointer
+
+            let newTokens = appendItem tokens newToken
+
+            let offsetFromToken =
+                let textLength = newToken.Text.Length
+
+                let offset =
+                    match newToken.Type with
+                    | TokenType.COMMENT
+                    | TokenType.STRING -> 2
+                    | _ -> 0
+
+                textLength + offset
+
+            let pointerOffset =
+                let tokenOffset = offsetFromToken
+                streamPointer + tokenOffset
+
+            match newToken.Type with
+            | TokenType.EOF -> newTokens
+            | _ -> lexLoop newTokens pointerOffset
+
+        lexLoop [||] 0
