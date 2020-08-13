5b76fb8ea4a30b03b7da2235e21ced8d9fb418b0
BaileyFirman <bailey.firman@nintex.com>
2020-08-13 18:00:33 +1200

    Initial usage of the better parser

--- Program.fs
+++ Program.fs
@@ -1,4 +1,5 @@
 open Lexer.Lexer
+open Parser.BetterParser
 open Types.Tokens
 open Microsoft.FSharp.Core
 
@@ -7,14 +8,18 @@ let main argv =
     let testString =
         "#Comment\n\t\r+-*/\n===>=><=< \"A String\" 1234 5.6789 PRINT REPEAT\n\u0004".ToCharArray()
 
-    lexCharacterStream testString
-    |> Array.map (fun x ->
-        let text =
-            match x.Text with
-            | "\n" -> "nl"
-            | _ -> x.Text
+    let tokenisedStream =
+        lexCharacterStream testString
+        |> Array.map (fun x ->
+            let text =
+                match x.Text with
+                | "\n" -> "nl"
+                | _ -> x.Text
 
-        let tokenType = x.Type.ToString()
-        printf "LEX: %s %s\n" tokenType text)
+            let tokenType = x.Type.ToString()
+            printf "LEX: %s %s\n" tokenType text)
+
+    parseTokenStream tokenisedStream
     |> ignore
+    
     0
