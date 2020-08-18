dfa50d478a778893885c3e4627ffdf39572b21f6
BaileyFirman <bailey.firman@nintex.com>
2020-08-18 21:27:48 +1200

    Re-implemented newline, printStatement, and statement

--- Parser.fs
+++ Parser.fs
@@ -15,20 +15,23 @@ module Parser =
 
     let parseTokenStream (tokenStream: Token []) =
         let rec newline streamPointer =
-            let currentToken = getToken tokenStream streamPointer
-            let currentTokenType = getType currentToken
-            let nextPointer = next streamPointer
+            // We need to assume newline is passed a
+            // pointer to the fist newline in a series
+            printfn "PARSE: STATEMENT-NEWLINE"
+            let nextStreamPointer = next streamPointer
+            let nextToken = getToken tokenStream nextStreamPointer
+            let nextTokenType = getType nextToken
 
-            match currentTokenType with
-            | TokenType.NEWLINE -> newline nextPointer
-            | _ -> nextPointer
+            match nextTokenType with
+            | TokenType.NEWLINE -> newline nextStreamPointer
+            | _ -> nextStreamPointer
 
         let expression = 1
 
         let rec printStatement streamPointer =
             // We know the previous token was print
             // becuase we called into printStatement
-            printf "PARSE: STATEMENT-PRINT"
+            printfn "PARSE: STATEMENT-PRINT"
             let currentToken = getToken tokenStream streamPointer
             let currentTokenType = getType currentToken
 
@@ -36,14 +39,17 @@ module Parser =
                 match currentTokenType with
                 | TokenType.STRING -> 1
                 | _ -> expression
-            mainOffset
+
+            let newStreamPointer = streamPointer + mainOffset
+            newline newStreamPointer
 
         let rec statement streamPointer =
             let currentToken = getToken tokenStream streamPointer
             let currentTokenType = getType currentToken
-
+            let nextStreamPointer = next streamPointer
+            
             match currentTokenType with
-            | TokenType.PRINT -> printStatement
+            | TokenType.PRINT -> printStatement nextStreamPointer
             | _ -> failwith "NOT IMPLEMENTED"
 
         let rec parseLoop streamPointer =
@@ -52,7 +58,12 @@ module Parser =
             let isEndOfStream = checkTokenType currentTokenType TokenType.EOF
 
             match isEndOfStream with
-            | true -> failwith "NO PARSING IMPLEMENTED"
-            | false -> statement streamPointer
+            | true -> 0
+            | false ->
+                streamPointer
+                |> statement
+                |> parseLoop
 
-        parseLoop 0
+        printfn "PARSE: START PARSING"
+        parseLoop 0 |> ignore
+        printfn "PARSE: END PARSING"
